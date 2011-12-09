b0f6583ab72bcbed232d6d86d40921e5a1a59b5a
Adrian Istrate <adr_istrate@yahoo.com>
2011-12-09 20:24:04 +0100

    first commit

---
+++ Chapter06/search.js
@@ -0,0 +1,39 @@
+var x = {
+    "completed_in": 0.098,
+    "max_id": 143027774595145728,
+    "max_id_str": "143027774595145728",
+    "next_page": "?page=2&max_id=143027774595145728&q=html5%20-RT&rpp=5",
+    "page": 1,
+    "query": "html5+-RT",
+    "refresh_url": "?since_id=143027774595145728&q=html5%20-RT",
+    "results": [{
+        "created_at": "Sat, 03 Dec 2011 18:04:13 +0000",
+        "from_user": "johnniemaneiro",
+        "from_user_id": 821380,
+        "from_user_id_str": "821380",
+        "from_user_name": "Johnnie Maneiro",
+        "geo": null,
+        "id": 143027774595145728,
+        "id_str": "143027774595145728",
+        "iso_language_code": "en",
+        "metadata": { "result_type": "recent" },
+        "profile_image_url": "http://a3.twimg.com/profile_images/1132445030/t_normal.png",
+        "source": "&lt;a href=&quot;http://twitter.com/tweetbutton&quot; rel=&quot;nofollow&quot;&gt;Tweet Button&lt;/a&gt;",
+        "text": "Check this website on #cssshot #html5  #jquery  http://t.co/X444V3xs via @clappsme @replaycreative",
+        "to_user": null,
+        "to_user_id": null,
+        "to_user_id_str": null,
+        "to_user_name": null
+    }, {
+        "created_at": "Sat, 03 Dec 2011 18:02:44 +0000", "from_user": "umessi10", "from_user_id": 91369589, "from_user_id_str": "91369589", "from_user_name": "Varun Nayak", "geo": null, "id": 143027400542920704, "id_str": "143027400542920704", "iso_language_code": "en", "metadata": { "result_type": "recent" }, "profile_image_url": "http://a0.twimg.com/profile_images/1217293164/DSC001252_2_normal.jpg", "source": "&lt;a href=&quot;http://twitter.com/&quot;&gt;web&lt;/a&gt;", "text": "http://t.co/JHhi9ei6 Visit here, Click on Join the HTML5 Trial & watch videos on the html5 player", "to_user": null, "to_user_id": null, "to_user_id_str": null, "to_user_name": null
+    }, {
+        "created_at": "Sat, 03 Dec 2011 18:02:33 +0000", "from_user": "jakepoletti", "from_user_id": 360773549, "from_user_id_str": "360773549", "from_user_name": "jake poletti", "geo": null, "id": 143027355340902400, "id_str": "143027355340902400", "iso_language_code": "eo", "metadata": { "result_type": "recent" }, "profile_image_url": "http://a0.twimg.com/profile_images/1665952075/threadless_jake_normal.jpg", "source": "&lt;a href=&quot;http://twitter.com/&quot;&gt;web&lt;/a&gt;", "text": "Magento Carbon HTML5 Magento design on Superhug. http://t.co/qP3Nr7VS", "to_user": null, "to_user_id": null, "to_user_id_str": null, "to_user_name": null
+    }, {
+        "created_at": "Sat, 03 Dec 2011 18:01:31 +0000", "from_user": "azmodeus", "from_user_id": 365266186, "from_user_id_str": "365266186", "from_user_name": "vicentegarcesramirez", "geo": null, "id": 143027093331116032, "id_str": "143027093331116032", "iso_language_code": "es", "metadata": { "result_type": "recent" }, "profile_image_url": "http://a0.twimg.com/profile_images/1521642341/IMG_0023_normal.JPG", "source": "&lt;a href=&quot;http://twitter.com/#!/download/iphone&quot; rel=&quot;nofollow&quot;&gt;Twitter for iPhone&lt;/a&gt;", "text": "Uuufffff parece que hoy tampoco habr\u00E1 tiempo para HTML5", "to_user": null, "to_user_id": null, "to_user_id_str": null, "to_user_name": null
+    }, {
+        "created_at": "Sat, 03 Dec 2011 18:01:13 +0000", "from_user": "SIKA_UMA", "from_user_id": 34518255, "from_user_id_str": "34518255", "from_user_name": "KABA", "geo": null, "id": 143027020023070720, "id_str": "143027020023070720", "iso_language_code": "ja", "metadata": { "result_type": "recent" }, "profile_image_url": "http://a0.twimg.com/profile_images/1342312744/character_normal.png", "source": "&lt;a href=&quot;http://twitter.com/tweetbutton&quot; rel=&quot;nofollow&quot;&gt;Tweet Button&lt;/a&gt;", "text": "\u3010MdN Design Interactive\u9023\u8F09\u9023\u8F09\u3000\u7B2C2\u56DE\u76EE\u3011xhtml\u304B\u3089HTML5\u3067\u4F7F\u3044\u65B9\u2026 - http://t.co/WIZczO80 \u3080\u3045\u3001\u3053\u3061\u3089\u306F p \u30BF\u30B0\u3067\u56F2\u308F\u308C\u3066\u3044\u308B\u3002\u3002", "to_user": null, "to_user_id": null, "to_user_id_str": null, "to_user_name": null
+    }],
+    "results_per_page": 5,
+    "since_id": 0,
+    "since_id_str": "0"
+};
